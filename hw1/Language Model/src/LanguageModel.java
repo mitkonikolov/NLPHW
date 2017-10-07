8043467685e0e4d08947fbffa993ae53a9018e51
mitkonikolov <nikolov.m@husky.neu.edu>
2017-10-07 14:37:16 -0400

    Split lines into separate words

--- hw1/Language Model/src/LanguageModel.java
+++ hw1/Language Model/src/LanguageModel.java
@@ -3,7 +3,10 @@ import com.opencsv.CSVReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.regex.Pattern;
 
 /**
  * Created by Mitko on 10/3/17.
@@ -12,11 +15,18 @@ public class LanguageModel {
 
     private HashMap<String, Integer> individualCounts;
     private HashMap<String, HashMap<String, Integer>> trigramCounts;
-    private int i=0;
+    private String trainingSet;
 
     public LanguageModel() {
         this.individualCounts = new HashMap<>();
         this.trigramCounts = new HashMap<>();
+        this.trainingSet = "train_set2.csv";
+    }
+
+    public LanguageModel(String trainingSetName) {
+        this.individualCounts = new HashMap<>();
+        this.trigramCounts = new HashMap<>();
+        this.trainingSet = trainingSetName;
     }
 
 
@@ -24,18 +34,38 @@ public class LanguageModel {
      *  Parses the given training corpus. It extracts the data from a csv file
      *  and calls another method in order to record the information.
      */
-    public void parseTrainingSet() {
+    public HashMap<Integer, List<String>> parseTrainingSet() {
+        HashMap<Integer, List<String>> result = new HashMap<>();
+
         try {
-            CSVReader reader = new CSVReader(new FileReader("train_set.csv"));
+            CSVReader reader = new CSVReader(new FileReader(this.trainingSet));
 
             String[] nextLine;
+            String[] temp;
 
             try {
                 // eliminate the first field that says "text"
+                // and skip the blank line under it
                 reader.readNext();
                 reader.readNext();
+                Pattern whiteSpaces = Pattern.compile("\\s*\\s");
+
+                int lineNumber = 0;
+
                 while ((nextLine = reader.readNext()) != null) {
-                    System.out.println(nextLine[8]);
+                    ArrayList<String> words = new ArrayList<>();
+
+                    temp = whiteSpaces.split(nextLine[8], 0);
+
+                    for(int i=0; i<temp.length; i++) {
+                        // accumulate all the words in a list
+                        words.add(temp[i]);
+                    }
+
+                    // put the list of words in the map
+                    result.put(lineNumber, words);
+                    lineNumber += 1;
+
                     // ignore the next line because it is empty
                     reader.readNext();
                 }
@@ -43,11 +73,13 @@ public class LanguageModel {
             catch (IOException e) {
                 e.getMessage();
             }
-
-        } catch (FileNotFoundException e) {
+        }
+        catch (FileNotFoundException e) {
             System.out.println("File was not found. Please check if the csv" +
-                    " file was in the described directory");
+                    " file was in the entered directory");
             e.printStackTrace();
         }
+
+        return result;
     }
 }
--- hw1/Language Model/test/TestLanguageModel.java
+++ hw1/Language Model/test/TestLanguageModel.java
@@ -1,14 +1,55 @@
 import org.junit.Test;
+import org.junit.Assert;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by Mitko on 10/3/17.
  */
 public class TestLanguageModel {
 
+    // Can the Language Model split the words
     @Test
-    public void testParsing() {
-        LanguageModel lm = new LanguageModel();
-        lm.parseTrainingSet();
+    public void testBasicParsing() {
+        LanguageModel lm = new LanguageModel("train_set1.csv");
+        HashMap<Integer, List<String>> r =  lm.parseTrainingSet();
+
+        List<String> line1 = new ArrayList<>();
+        line1.add("Okay.");
+        line1.add("/");
+
+        List<String> line2 = new ArrayList<>();
+        line2.add("{D");
+        line2.add("So,");
+        line2.add("}");
+
+        List<String> line3 = new ArrayList<>();
+        line3.add("[");
+        line3.add("[");
+        line3.add("I");
+        line3.add("guess,");
+        line3.add("+");
+
+        HashMap<Integer, List<String>> expected = new HashMap<>();
+        expected.put(0, line1);
+        expected.put(1, line2);
+        expected.put(2, line3);
+
+        for(int i = 0; i<r.size(); i++) {
+            List<String> actualLine = r.get(i);
+            List<String> expectedLine = expected.get(i);
+
+            assertEquals("Lists' length",
+                    expectedLine.size(), actualLine.size());
+
+            for(int p = 0; p<actualLine.size(); p++) {
+                assertEquals(actualLine.get(p), expectedLine.get(p));
+            }
+        }
     }
 
 }
