be3c403c5bbdd67293e45e337bde4459ab342fb8
mitkonikolov <nikolov.m@husky.neu.edu>
2017-10-10 12:37:03 -0400

    Updated testing

--- hw1/POS Tagger/test/TestPOSTagger.java
+++ hw1/POS Tagger/test/TestPOSTagger.java
@@ -24,7 +24,7 @@ public class TestPOSTagger {
         //printWordTagCount(wordTags);
         //printUnigramCount(unigramCount);
         //printWordTagCount(bigramCount);
-        //printTrigramCount(trigramCount);
+        printTrigramCount(trigramCount);
 
     }
 
