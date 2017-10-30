60bfd905cae397ae50b6a41c267290ee52657bd3
mitkonikolov <nikolov.m@husky.neu.edu>
2017-10-30 01:52:23 -0400

    Problem 5 finished

---
+++ hw2/Sentiment Analysis/src/MultPerceptron.java
@@ -0,0 +1,19 @@
+import org.tensorflow.Tensor;
+import org.tensorflow.TensorFlow;
+
+/**
+ * Created by Mitko on 10/29/17.
+ */
+public class MultPerceptron {
+    TensorFlow tf;
+
+    public MultPerceptron() {
+
+    }
+
+/*    public void setUp() {
+        Tensor t = Tensor();
+    }*/
+
+
+}
